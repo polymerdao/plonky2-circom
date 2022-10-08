f00c2796cb2eb7070a2e75b40d7eef4f9d8a8491
Sai Deng <dengsai@hotmail.com>
2022-10-07 19:25:16 -0700

    Add e2e test from circom circuits to solidity verifier

---
+++ circom/e2e_tests/run.sh
@@ -0,0 +1,9 @@
+CIRCUIT_NAME=plonk
+POT_PATH=~/Downloads/powersOfTau28_hez_final_12.ptau
+circom ./${CIRCUIT_NAME}.circom --r1cs --wasm --sym
+snarkjs groth16 setup $CIRCUIT_NAME.r1cs ${POT_PATH} circuit_0000.zkey
+node ./${CIRCUIT_NAME}_js/generate_witness.js ./${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ./input.json ./witness.wtns
+snarkjs groth16 prove circuit_0000.zkey ./witness.wtns proof.json public.json
+snarkjs zkey export solidityverifier circuit_0000.zkey verifier.sol
+snarkjs zkey export verificationkey circuit_0000.zkey verification_key.json
+snarkjs groth16 verify verification_key.json public.json proof.json
