06313c7f088fdca567f507529d3db8d44351682f
Sai Deng <dengsai@hotmail.com>
2022-10-13 07:14:33 -0700

    Goldilocks Poseidon init

---
+++ circom/test/poseidon.test.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const wasm_tester = require("circom_tester").wasm;
+
+describe("Poseidon Circuit Test", function () {
+    let circuit;
+
+    this.timeout(10000000);
+
+    before(async () => {
+        circuit = await wasm_tester(path.join(__dirname, "circuits", "poseidon.test.circom"), {});
+    });
+
+    it("Should pass", async () => {
+        const input = {
+            in: 1
+        };
+
+        const w = await circuit.calculateWitness(input, true);
+
+        await circuit.assertOut(w, {out: 1});
+    });
+});
