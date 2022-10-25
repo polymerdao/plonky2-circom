a3b95bd6327f01c4b37e77ca735f64d4b5a42845
Sai Deng <dengsai@hotmail.com>
2022-10-25 15:36:54 -0700

    update e2e run.sh

--- circom/e2e_tests/run.sh
+++ circom/e2e_tests/run.sh
@@ -1,9 +1,9 @@
 CIRCUIT_NAME=e2e
 POT_PATH=~/Downloads/powersOfTau28_hez_final_21.ptau
-circom ./${CIRCUIT_NAME}.circom --r1cs --wasm --sym
-snarkjs groth16 setup $CIRCUIT_NAME.r1cs ${POT_PATH} circuit_0000.zkey
+circom ./${CIRCUIT_NAME}.circom --r1cs --wasm --sym -p goldilocks
 node ./${CIRCUIT_NAME}_js/generate_witness.js ./${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ./input.json ./witness.wtns
-snarkjs groth16 prove circuit_0000.zkey ./witness.wtns proof.json public.json
-snarkjs zkey export solidityverifier circuit_0000.zkey verifier.sol
-snarkjs zkey export verificationkey circuit_0000.zkey verification_key.json
-snarkjs groth16 verify verification_key.json public.json proof.json
+#snarkjs groth16 setup $CIRCUIT_NAME.r1cs ${POT_PATH} circuit_0000.zkey
+#snarkjs groth16 prove circuit_0000.zkey ./witness.wtns proof.json public.json
+#snarkjs zkey export solidityverifier circuit_0000.zkey verifier.sol
+#snarkjs zkey export verificationkey circuit_0000.zkey verification_key.json
+#snarkjs groth16 verify verification_key.json public.json proof.json
