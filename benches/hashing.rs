72b31fe2be65046af1c8a4395d66e6d42cbea071
Sai Deng <dengsai@hotmail.com>
2022-11-01 10:06:02 -0700

    Add hashing benches

---
+++ benches/hashing.rs
@@ -0,0 +1,42 @@
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use plonky2::field::goldilocks_field::GoldilocksField;
+use plonky2::hash::hash_types::RichField;
+use plonky2::hash::hashing::PlonkyPermutation;
+use plonky2::hash::hashing::SPONGE_WIDTH;
+use plonky2::hash::poseidon::{PoseidonPermutation};
+use plonky2_circom_verifier::config::PoseidonBN128Permutation;
+use std::any::type_name;
+
+pub(crate) fn bench_gl_poseidon<F: RichField>(c: &mut Criterion) {
+    c.bench_function(
+        &format!("goldilocks poseidon<{}, {SPONGE_WIDTH}>", type_name::<F>()),
+        |b| {
+            b.iter_batched(
+                || F::rand_arr::<SPONGE_WIDTH>(),
+                |state| PoseidonPermutation::permute(state),
+                BatchSize::SmallInput,
+            )
+        },
+    );
+}
+
+pub(crate) fn bench_bn128_poseidon<F: RichField>(c: &mut Criterion) {
+    c.bench_function(
+        &format!("bn128 poseidon<{}, {SPONGE_WIDTH}>", type_name::<F>()),
+        |b| {
+            b.iter_batched(
+                || F::rand_arr::<SPONGE_WIDTH>(),
+                |state| PoseidonBN128Permutation::permute(state),
+                BatchSize::SmallInput,
+            )
+        },
+    );
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    bench_gl_poseidon::<GoldilocksField>(c);
+    bench_bn128_poseidon::<GoldilocksField>(c);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
