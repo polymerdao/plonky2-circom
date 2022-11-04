a88d64e7a1d2ebc28088e2706b8b50ef59123221
sai <dengsai@hotmail.com>
2022-11-03 20:40:49 -0700

    fix e2e test

--- circom/e2e_tests/run.sh
+++ circom/e2e_tests/run.sh
@@ -3,7 +3,8 @@ CIRCUIT_PATH=../circuits/plonky2.circom
 INPUT_PATH=../test/data/proof.json
 POT_PATH=~/Downloads/powersOfTau28_hez_final_24.ptau
 RAPIDSNARK_PATH=../../../rapidsnark/build/prover
-NODE_PATH=~/node/out/Release/node
+# node version > 18
+NODE_PATH=node
 SNARKJS_PATH=../../../../snarkjs/cli.js
 NODE_PARAMS="--trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc"
 
@@ -15,46 +16,56 @@ echo "DONE ($((end - start))s)"
 
 echo "****COMPILING WITNESS GENERATOR****"
 start=$(date +%s)
-cd ${CIRCUIT_NAME}_cpp
-make -j
-cd ..
+cd ${CIRCUIT_NAME}_cpp && make -j && cd ..
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
 echo "****GENERATING ZKEY 0****"
 # If failed: https://hackmd.io/@yisun/BkT0RS87q
 start=$(date +%s)
-${NODE_PATH} ${NODE_PARAMS} ${SNARKJS_PATH} groth16 setup $CIRCUIT_NAME.r1cs ${POT_PATH} circuit_0000.zkey
+#${NODE_PATH} ${NODE_PARAMS} ${SNARKJS_PATH} groth16 setup $CIRCUIT_NAME.r1cs ${POT_PATH} "$CIRCUIT_NAME"_0.zkey
+${NODE_PATH} ${NODE_PARAMS} ${SNARKJS_PATH} zkey new $CIRCUIT_NAME.r1cs ${POT_PATH} "$CIRCUIT_NAME"_0.zkey -v > zkey0.out
+end=$(date +%s)
+echo "DONE ($((end - start))s)"
+
+echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
+start=$(date +%s)
+${NODE_PATH} ${NODE_PARAMS} ${SNARKJS_PATH} zkey contribute -verbose "$CIRCUIT_NAME"_0.zkey "$CIRCUIT_NAME".zkey -n="First phase2 contribution" -e="some random text" > contribute.out
+end=$(date +%s)
+echo "DONE ($((end - start))s)"
+
+echo "****VERIFYING FINAL ZKEY****"
+start=$(date +%s)
+${NODE_PATH} ${NODE_PARAMS} ${SNARKJS_PATH} zkey verify -verbose "$CIRCUIT_NAME".r1cs ${POT_PATH} "$CIRCUIT_NAME".zkey > verify.out
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
 echo "****EXPORTING VKEY****"
 start=$(date +%s)
-${NODE_PATH} ${SNARKJS_PATH} zkey export verificationkey circuit_0000.zkey verification_key.json
+${NODE_PATH} ${SNARKJS_PATH} zkey export verificationkey "$CIRCUIT_NAME".zkey verification_key.json -v
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
 echo "****WITNESS GENERATION****"
 start=$(date +%s)
-#${NODE_PATH} ./${CIRCUIT_NAME}_js/generate_witness.js ./${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ${INPUT_PATH} ./witness.wtns
 ./${CIRCUIT_NAME}_cpp/${CIRCUIT_NAME} ${INPUT_PATH} ./witness.wtns
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
 echo "****GENERATING PROOF****"
 start=$(date +%s)
-${RAPIDSNARK_PATH} circuit_0000.zkey ./witness.wtns proof.json public.json
-#${NODE_PATH} ${SNARKJS_PATH} groth16 prove circuit_0000.zkey ./witness.wtns proof.json public.json
+${RAPIDSNARK_PATH} "$CIRCUIT_NAME".zkey ./witness.wtns proof.json public.json
+#${NODE_PATH} ${SNARKJS_PATH} groth16 prove "$CIRCUIT_NAME".zkey ./witness.wtns proof.json public.json
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
 echo "****VERIFYING PROOF****"
 # If failed: double check the i/o of the circuits
 start=$(date +%s)
-${NODE_PATH} ${SNARKJS_PATH} groth16 verify verification_key.json public.json proof.json
+${NODE_PATH} ${SNARKJS_PATH} groth16 verify verification_key.json public.json proof.json -v
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
-${NODE_PATH} ${SNARKJS_PATH} zkey export solidityverifier circuit_0000.zkey verifier.sol
+${NODE_PATH} ${SNARKJS_PATH} zkey export solidityverifier "$CIRCUIT_NAME".zkey verifier.sol
 ${NODE_PATH} ${SNARKJS_PATH} generatecall public.json
 cp verifier.sol ./hardhat/contracts
